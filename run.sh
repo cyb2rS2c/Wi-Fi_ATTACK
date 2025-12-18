df9bc239abcf3777a51c2c3368a47ce7cf4076e4
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-12-18 13:19:13 +0100

    Create run.sh (pip installation)

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "$EUID" -ne 0 ]]; then
+  exec sudo "$0" "$@"
+fi
+
+python3 -m venv testenv; source testenv/bin/activate
+pip install Wi-Fi-Attack==4.0.1
+exec  wifi-cracker 
